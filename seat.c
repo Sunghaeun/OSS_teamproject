e073e18c01cc55ba81a0b350e57213505c35ff2c
Your Name <you@example.com>
2023-05-07 00:03:03 +0900

    create seat.c

---
+++ seat.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+typedef struct{
+  char name[20];
+  int time;
+  int seat;
+
+}user;
+void seat_create();
+void seat_read();
+void seat_change();
+void seat_delete();
+void searchName();
+void timeAdd();
+void orderFood();
+void report_seat();
+
+int main(void) {
+  
+  return 0;
+}
+
+void seat_create(){}
+void seat_read(){}
+void seat_change(){}
+void seat_delete(){}
+void searchName(){}
+void timeAdd(){}
+void orderFood(){}
+void report_seat(){}
