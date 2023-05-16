fbf15d5fcd27983c723cb44d34f0f2014655b8c3
yohan-seo <126676826+yohan-seo@users.noreply.github.com>
2023-05-17 06:37:24 +0900

    Update other.c

--- other.c
+++ other.c
@@ -1,2 +1,18 @@
 //crud외 다른 기능
 #include "other.h"
+
+void searchName(){
+}
+void timeAdd(){
+}
+void orderFood(){
+}
+void report_seat(){
+}
+void event(){
+  
+}
+void file_store(){
+}
+int file_import(){
+}
