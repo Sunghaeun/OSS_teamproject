217cb2d6d60244ed0a42a7c94d4832154c00f6ba
yohan-seo <126676826+yohan-seo@users.noreply.github.com>
2023-05-16 16:58:56 +0900

    Update pc_room.c

--- pc_room.c
+++ pc_room.c
@@ -1,49 +1,53 @@
-#include "pc_room.h"
+#include <stdio.h>
+#include "menu.h"
+#include "other.h"
+#include "CRUD.h"
 //main 함수
 int main(void) {
   file_import();
   
   int menu = start();
-  
-  which(menu){
-    case 0: 
-      file_store();
-      return 0;
-    
-    case 1:
-      seat_create();
-      break;
-    
-    case 2:
-      seat_read();
-      break;
-    
-    case 3:
-      seat_change();
-      break;
-    
-    case 4:
-      seat_delete();
-      break;
-    
-    case 5:
-      searchName();
-      break;
-    
-    case 6:
-      timeAdd();
-      break;
-    
-    case 7:
-      orderFood();
-      break;
-    
-    case 8:
-      report_seat();
-      break;
-    
-    default:
-     break;
+  while(1){
+    which(menu){
+      case 0: 
+        file_store();
+        return 0;
+
+      case 1:
+        seat_create();
+        break;
+
+      case 2:
+        seat_read();
+        break;
+
+      case 3:
+        seat_change();
+        break;
+
+      case 4:
+        seat_delete();
+        break;
+
+      case 5:
+        searchName();
+        break;
+
+      case 6:
+        timeAdd();
+        break;
+
+      case 7:
+        orderFood();
+        break;
+
+      case 8:
+        report_seat();
+        break;
+
+      default:
+       break;
+    }
   }
   return 0;
 }
