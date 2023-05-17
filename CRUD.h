58bf8e3ac5b946d2024e0c6a6cdc694763999c8e
Sunghaeun <130740217+Sunghaeun@users.noreply.github.com>
2023-05-17 14:21:59 +0900

    Update CRUD.h

--- CRUD.h
+++ CRUD.h
@@ -1,3 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct{
+  char name[20];
+  int time;
+  int seat[10]={0}; 
+}user;
+
 void seat_create();
 void seat_read();
 void seat_change();
