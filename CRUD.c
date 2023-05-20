1dbe820e20a66bb8df226be7759eee3c2c180de5
Sunghaeun <130740217+Sunghaeun@users.noreply.github.com>
2023-05-20 21:28:28 +0900

    Update CRUD.c

--- CRUD.c
+++ CRUD.c
@@ -14,10 +14,27 @@ void seat_create(user *u){
     return 1;
 }
 
-void seat_read(user *u){
-     printf("%-6s %3d원\n",u->name,u->time);
+void seat_read(user u){
+     printf("%-6s %3d원\n",u.name,u.time);
 }
-
+//listScore2, selectDataNo2적용 잘해야됨
+void listScore2(user *u[], int count){
+    printf("\n No 이름  이용시간\n");
+    printf("==========================================================\n");
+    for(int i=0;i<count;i++){
+        if(u[i]==NULL) continue;
+        printf("%2d ", i+1);
+        seat_read(*u[i]);
+    }
+    printf("\n");
+}
+int selectDataNo2(user *u[], int count){
+    int no;
+    listScore2(u, count);
+    printf("번호는 (취소 :0)? ");
+    scanf("%d", &no);
+    return no;
+}
 void seat_change(user *u){
     printf("이름을 입력하세요. ");
     scanf("%d",&u->name);
