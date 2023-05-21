434fe0685490725a4bde03042fb144ff0945c81a
Sunghaeun <130740217+Sunghaeun@users.noreply.github.com>
2023-05-21 23:13:22 +0900

    Update CRUD.c

--- CRUD.c
+++ CRUD.c
@@ -1,11 +1,71 @@
 //crud기능
 #include "CRUD.h" 
  
-void seat_create(user *u){
-    printf("이름을 입력하세요. ");
-    scanf("%d",&u->name);
+void seating_sheat(user u){
+    int seat_n=0;
+    //첫째줄 시작
+    for(int i=0;i<36;i++){
+        printf("-");
+    }
+    printf("\n");
+    for(int i=0;i<27;i++){
+        if(i%5==0) printf("|");
+        if(i%5==3) {
+          seat_n++;
+          if(seat_n==u.seat){
+            printf("X");
+          } else printf("%d",seat_n);
+          }
+        printf(" ");
+    }
+    printf("\n");
+    for(int i=0;i<36;i++){
+        printf("-");
+    }
+    //첫째줄 끝
+    printf("\n");
+    //왼쪽 세로 시작
+    for(int j=0;j<2;j++){
+        for(int i=0;i<8;i++){
+            if(i%5==0) printf("|");
+            if(i%5==3) printf("%d",++seat_n);
+            printf(" ");
+        }
+        printf("\n");
+        for(int i=0;i<8;i++){
+            printf("-");
+        }
+        printf("\n");
+    }
+    for(int i=0;i<8;i++){
+            if(i%5==0) printf("|");
+            if(i%5==3) printf("%d",++seat_n);
+            printf(" ");
+        }
+        printf("\n");
+    //왼쪽 세로 끝
+    for(int i=0;i<36;i++){
+        printf("-");
+    }
+    printf("\n");
+    for(int i=0;i<27;i++){
+        if(i==2) printf(" ");
+        if(i%5==0) printf("|");
+        if(i%5==2) printf("%d",++seat_n);
+        else printf(" ");
+    }
+    printf("\n");
+    for(int i=0;i<36;i++){
+        printf("-");
+    }
+    printf("\n");
+}
 
-    printf("아용시간을 입력하세요. ");
+int seat_create(user *u){
+    printf("이름을 입력하세요. ");
+    scanf("%s",&u->name);
+
+    printf("이용시간을 입력하세요. ");
     scanf("%d",&u->time);
     
     u->seat=1;
@@ -14,8 +74,8 @@ void seat_create(user *u){
     return 1;
 }
 
-void seat_read(user u){
-     printf("%-6s %3d원\n",u.name,u.time);
+void seat_read(user *u){
+     printf("%-6s %3d원\n",u->name,u->time);
 }
 //listScore2, selectDataNo2적용 잘해야됨
 void listScore2(user *u[], int count){
@@ -24,7 +84,7 @@ void listScore2(user *u[], int count){
     for(int i=0;i<count;i++){
         if(u[i]==NULL) continue;
         printf("%2d ", i+1);
-        seat_read(*u[i]);
+        seat_read(u[i]);
     }
     printf("\n");
 }
@@ -37,18 +97,10 @@ int selectDataNo2(user *u[], int count){
 }
 void seat_change(user *u){
     printf("이름을 입력하세요. ");
-    scanf("%d",&u->name);
+    scanf("%s",&u->name);
 
     printf("이용시간을 입력하세요. ");
     scanf("%d",&u->time);
     
     printf("==> 수정되었습니다!\n");
-    return 1;
-}
-
-void seat_delete(user *u){
-    u->time=-1;
-    u->seat=0;
-    printf("==> 삭제되었습니다!\n");
-    return 0;
 }
