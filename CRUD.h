21e018d25762bbe1be22a2b718f9e2a7f4bda1dc
yohan-seo <126676826+yohan-seo@users.noreply.github.com>
2023-05-22 21:46:05 +0900

    Update CRUD.h

--- CRUD.h
+++ CRUD.h
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct{
-  char name[20];
+typedef struct {
+  char name[8];
   int time;
-  int seat=0;
-  char food[20];
-  //좋아하는 게임 장르나 나이를 입력 받아서 10시 이후까지 이용 예정이라면 금지 메세지 띄우기
-}user; 
+  int seat;
+  int food[20];
+} schedule;
 
 void seat_create(user *u);
 void seat_read(user u);
