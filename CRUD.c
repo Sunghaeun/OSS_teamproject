7965c0781254122368449a94c08fcff4a3260dd2
yohan-seo <126676826+yohan-seo@users.noreply.github.com>
2023-05-22 21:54:57 +0900

    Update CRUD.c

--- CRUD.c
+++ CRUD.c
@@ -1,110 +1,79 @@
 //crud기능
 #include "CRUD.h" 
  
-void seating_sheat(user u){
-    int seat_n=0;
-    //첫째줄 시작
-    for(int i=0;i<36;i++){
-        printf("-");
-    }
-    printf("\n");
-    for(int i=0;i<27;i++){
-        if(i%5==0) printf("|");
-        if(i%5==3) {
-          seat_n++;
-          if(seat_n==u.seat){
-            printf("X");
-          } else printf("%d",seat_n);
-          }
-        printf(" ");
-    }
-    printf("\n");
-    for(int i=0;i<36;i++){
-        printf("-");
-    }
-    //첫째줄 끝
-    printf("\n");
-    //왼쪽 세로 시작
-    for(int j=0;j<2;j++){
-        for(int i=0;i<8;i++){
-            if(i%5==0) printf("|");
-            if(i%5==3) printf("%d",++seat_n);
-            printf(" ");
-        }
-        printf("\n");
-        for(int i=0;i<8;i++){
-            printf("-");
-        }
-        printf("\n");
-    }
-    for(int i=0;i<8;i++){
-            if(i%5==0) printf("|");
-            if(i%5==3) printf("%d",++seat_n);
-            printf(" ");
-        }
-        printf("\n");
-    //왼쪽 세로 끝
-    for(int i=0;i<36;i++){
-        printf("-");
-    }
-    printf("\n");
-    for(int i=0;i<27;i++){
-        if(i==2) printf(" ");
-        if(i%5==0) printf("|");
-        if(i%5==2) printf("%d",++seat_n);
-        else printf(" ");
-    }
-    printf("\n");
-    for(int i=0;i<36;i++){
-        printf("-");
-    }
-    printf("\n");
+int addScore(schedule *s, int pc_seat[]) {
+  printf("몇번 자리를 선택하시겠습니까? ");
+  scanf("%d", &s->seat);
+  while (pc_seat[s->seat - 1] != 0) {
+  printf("그 자리는 선택 불가능합니다.\n");
+  printf("몇번 자리를 선택하시겠습니까? ");
+  scanf("%d", &s->seat);
 }
 
-int seat_create(user *u){
-    printf("이름을 입력하세요. ");
-    scanf("%s",&u->name);
-
-    printf("이용시간을 입력하세요.(분) ");
-    scanf("%d",&u->time);
-    
-    u->seat=1;
- 
- if(&u->time > 120){
-  event();
- }
-    
-    printf("==> 추가되었습니다!\n"); 
-    return 1;
+  
+  printf("\n이름은? ");
+  scanf("%s", s->name);
+  printf("시간은? ");
+  scanf("%d", &s->time);
+  return 1;
+}
+void readScore(schedule s) {
+  printf("%5s %3d분 %3d번 좌석\n", s.name, s.time, s.seat);
 }
 
-void seat_read(user *u){
-     printf("%-6s %3d원\n",u->name,u->time);
+void listScore2(schedule *s[], int count) {
+  printf("\n No 이름  시간  선택 좌석\n");
+  printf("==========================================================\n");
+  for (int i = 0; i < count; i++) {
+    if (s[i] == NULL)
+      continue;
+    printf("%2d ", i + 1);
+    readScore(*s[i]);
+  }
+  printf("\n");
 }
-//listScore2, selectDataNo2적용 잘해야됨
-void listScore2(user *u[], int count){
-    printf("\n No 이름  이용시간\n");
+void listScore3(int pc_seat[]) {
+  int j = 0;
+  for (int i = 0; i < 13; i++) {
+    if (pc_seat[i] != 0)
+      j++;
+  }
+  if (j != 0) {
+    printf("\n선택 불가능한 자리\n");
     printf("==========================================================\n");
-    for(int i=0;i<count;i++){
-        if(u[i]==NULL) continue;
-        printf("%2d ", i+1);
-        seat_read(u[i]);
+    for (int i = 0; i < 13; i++) {
+      if (pc_seat[i] != 0)
+        printf("%d번\n", i+1);
     }
-    printf("\n");
+    printf("==========================================================\n");
+  }
 }
-int selectDataNo2(user *u[], int count){
-    int no;
-    listScore2(u, count);
-    printf("번호는 (취소 :0)? ");
-    scanf("%d", &no);
-    return no;
-}
-void seat_change(user *u){
-    printf("이름을 입력하세요. ");
-    scanf("%s",&u->name);
 
-    printf("이용시간을 입력하세요. ");
-    scanf("%d",&u->time);
-    
-    printf("==> 수정되었습니다!\n");
+
+void updateScore(schedule *s[], int index) {
+  int choice = 0;
+  printf("몇번 자리를 선택하시겠습니까? ");
+  scanf("%d", &choice);
+  for (int i = 0; i < 15; i++) {
+    if (s[i]->seat == choice) {
+      printf("이미 선택된 좌석입니다!\n");
+      break;
+    } else {
+      s[index]->seat = choice;
+      printf("=> 수정성공!\n");
+      break;
+    }
+  }
+}
+int deleteScore(schedule *s) {
+  s->time = -1;
+  printf("=> 삭제됨!\n");
+}
+
+int selectDataNo2(schedule *s[], int count) {
+  int no;
+  listScore2(s, count);
+  printf("번호는 (취소 :0)? ");
+  scanf("%d", &no);
+  return no;
 }
