13edc1defd309617cd7f580513ca001fbc1a23f1
yohan-seo <126676826+yohan-seo@users.noreply.github.com>
2023-05-23 23:48:48 +0900

    Update menu.c

--- menu.c
+++ menu.c
@@ -4,10 +4,10 @@
 int selectMenu() {
   int menu;
   printf("\n*** 점수계산기 ***\n");
-  printf("1. 로그인C\n");
-  printf("2. 정보조회R\n");
-  printf("3. 자리이동U\n");
-  printf("4. 로그아웃D\n");
+  printf("1. 로그인\n");
+  printf("2. 정보조회\n");
+  printf("3. 자리이동\n");
+  printf("4. 로그아웃\n");
   printf("5. 이름검색\n");
   printf("6. 시간 추가\n");
   printf("7. 음식 주문\n");
@@ -20,4 +20,3 @@ int selectMenu() {
   scanf("%d", &menu);
   return menu;
 }
-
